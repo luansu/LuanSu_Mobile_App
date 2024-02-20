6e0a97b20c9b4da772daabc509917b2971d700dc
luansu <sm.luan2003@gmail.com>
2024-02-20 21:15:47 +0700

    fisrt cmt

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet} from 'react-native'
+import React from 'react'
+import CustomIcon from '../components/CustomIcon'
+
+const HomeScreen = () => {
+  return (
+    <CustomIcon name='home' size={100}></CustomIcon>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
